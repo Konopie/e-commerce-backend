38e1f6d83ae9814ffd867a251d92b4b32df8905e
Cody Cooper <codycooper06@gmail.com>
2022-08-22 23:50:19 -0400

    got sequelize working

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 // import sequelize connection
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session(sess));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({force: true}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+})
