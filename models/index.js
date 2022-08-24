4a99bd763b31f22a751121d9acb001c7113f6cc9
Cody Cooper <codycooper06@gmail.com>
2022-08-24 16:16:18 -0400

    models complete

--- models/index.js
+++ models/index.js
@@ -7,13 +7,30 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL'
+  onDelete: 'CASCADE'
 })
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  as: 'productTag',
+
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  as: 'productTag',
+
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
 
 module.exports = {
   Product,
