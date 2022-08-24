316b87452f9754d12e818ce9392874d53bc63dc7
Cody Cooper <codycooper06@gmail.com>
2022-08-24 16:02:47 -0400

    models complete

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_id: {
+      type: Datatype.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataType.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,14 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll()
+  .then(categories => {
+    res.json(categories)
+  }).catch(err =>{
+    console.log(err)
+    res.status(500).json(err);
+  })
+
   // be sure to include its associated Products
 });
 
