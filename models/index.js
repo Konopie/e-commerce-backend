5c1f8e207df11458f74ea7afeef1bbc4165fe53d
Cody Cooper <codycooper06@gmail.com>
2022-08-24 18:24:33 -0400

    got seeds to run

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  as: 'product_id',
+  as: 'tag_id',
 
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
@@ -26,7 +26,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
-  as: 'tag_id',
+  as: 'product_id',
 
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
